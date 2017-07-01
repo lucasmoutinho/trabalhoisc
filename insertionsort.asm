83fd7ba7422fbc53d2b18c5f8f45f6716bbb9c5a
Lucas Moutinho <lucasmoutinho>
2017-07-01 03:28:08 -0300

    criado insertionsort.asm

--- insertionsort.asm
+++ insertionsort.asm
@@ -4,28 +4,32 @@
 	message: .asciiz "Fim loop"
 .text
 
+# $t7 = elemento do vetor
+# $t0 = indice i (leitura do vetor)
+# $t1 = indice k (percorre todo o vetor)
+# $t2 = indice m (indice menor)
+# $t4
+
 main:  
 	li $v0, 4 #mensagem para entrada:
 	la $a0, prompt
 	syscall 
 	addi $t0, $zero, 0 #zerando a variavel de controle do loop:
 	
-while: #lopp:
+input: #lopp:
 	bgt $t0, 16, continue #condicao
 	li $v0, 5 #Input
 	syscall #tacando input em v0:
 	move $t7, $v0 #colando input no vetor:
-	sw $t7, array1($t0) #incremento loop:
-	addi $t0, $t0, 4
-	j while
+	sw $t7, array1($t0) #$t7 = elemento do vetor
+	addi $t0, $t0, 4 #incremento loop:
+	j input
 	
 continue: 
+	addi $t1, $zero, 0
 	
-	addi $t3, $zero, 0
-	j while2
-	
-while2:
-	bgt $t3, 16, continue2 #menor:
+ordena: 
+	bgt $t1, 16, continue2 #menor:
 	addi $t0, $t3, 0
 	addi $t2, $t0, 0
 		
@@ -41,26 +45,26 @@ posicaomenor:
 	addi $t2, $t0, 0
 				
 continue3: #fim menor
-	lw $t9, array1($t3) #troca:
+	lw $t9, array1($t1) #troca:
 	lw $t8, array1($t2)
 	sw $t9, array1($t2)
-	sw $t8, array1($t3) #fim troca
-	addi $t3, $t3, 4
+	sw $t8, array1($t1) #fim troca
+	addi $t1, $t1, 4
 	j while2	
 		
 continue2:	
 	addi $t0, $zero, 0
 		
-while4:
-	bgt $t0, 16, exit4
-	lw $t1, array1($t0)
+imprime:
+	bgt $t0, 16, end
+	lw $t7, array1($t0)
 	li $v0, 1
-	addi $a0, $t1, 0
+	addi $a0, $t7, 0
 	syscall
 	addi $t0, $t0, 4
-	j while4
+	j imprime
 	
-exit4:
+end:
 	li $v0, 10
 	syscall
 
